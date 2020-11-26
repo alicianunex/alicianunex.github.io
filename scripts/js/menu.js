f2cd0de0053ab0ad26fe1fcbdfe7f05b9c040938
alicianunezisaac <alicianunez10492@gmail.com>
2020-11-26 22:53:36 +0100

    stylishing

---
+++ scripts/js/menu.js
@@ -0,0 +1,28 @@
+(function () {
+  var bodyEl = document.body,
+    openbtn = document.getElementById('open-button'),
+    closebtn = document.getElementById('close-button'),
+    isOpen = false;
+
+  function init() {
+    initEvents();
+  }
+
+  function initEvents() {
+    openbtn.addEventListener('click', toggleMenu);
+    if (closebtn) {
+      closebtn.addEventListener('click', toggleMenu);
+    }
+  }
+
+  function toggleMenu() {
+    if (isOpen) {
+      classie.remove(bodyEl, 'show-menu');
+    } else {
+      classie.add(bodyEl, 'show-menu');
+    }
+    isOpen = !isOpen;
+  }
+
+  init();
+})();
